29e19efe5803dfd9e2e199a317a04c32f19984d1
rcooke-ast <rcooke.ast@gmail.com>
2019-03-23 21:45:02 +0000

    first commit

---
+++ fit_cube.py
@@ -0,0 +1,151 @@
+import os
+import pdb
+import numpy as np
+from alis.alis import alis
+from alis.alsave import save_model as get_alis_string
+
+def make_modeldata(freq, flux, flue, p0):
+    if not os.path.exists("tempdata"):
+        os.mkdir("tempdata")
+    sname = "tempdata/spectrum.dat"
+    np.savetxt(sname, np.transpose((freq, flux, flue)))
+    parlines = get_parlines()
+    datlines = get_datlines(sname)
+    modlines = get_modlines(p0)
+    return parlines, datlines, modlines
+
+def get_datlines(sname, kind="hanning"):
+    datlines = []
+    shtxt = "0.0SFIX"
+    datlines += ["  {0:s}  specid=0  fitrange=columns  loadrange=all  resolution=apod(kind:{1:s}) shift=vshift({2:s}) columns=[wave:0,flux:1,error:2]".format(sname, res, shtxt)]
+    return datlines
+
+def get_modlines(p0):
+    modlines = []
+    # Do the emission
+    modlines += ["emission"]
+    modlines += ["legendre  0.0 0.0  scale=1.0,1.0  specid=0"]
+    modlines += ["gaussian  {0:f}   {1:f}   {2:f}  specid=0".format(p0[0], p0[1], p0[2])]
+    return modlines
+
+def get_parlines():
+    parlines = []
+    parlines += ["run  ncpus  3"]
+    parlines += ["run ngpus 0"]
+    parlines += ["run nsubpix 5"]
+    parlines += ["run blind False"]
+    parlines += ["run convergence False"]
+    parlines += ["run convcriteria 0.2"]
+    parlines += ["run bintype Hz"]
+    parlines += ["chisq atol 0.001"]
+    parlines += ["chisq xtol 0.0"]
+    parlines += ["chisq ftol 0.0"]
+    parlines += ["chisq gtol 0.0"]
+    parlines += ["chisq fstep 1.3"]
+    parlines += ["chisq miniter 10"]
+    parlines += ["chisq maxiter 1000"]
+    parlines += ["out model False"]
+    parlines += ["out fits False"]
+    parlines += ["out verbose -1"]
+    parlines += ["out overwrite True"]
+    parlines += ["plot fits False"]
+    return parlines
+
+def parse_fit(result):
+    pars, errs = np.zeros(3), np.zeros(3)
+    # Convert the results into an easy read format
+    fres = result._fitresults
+    info = [(result._tend - result._tstart)/3600.0, fres.fnorm, fres.dof, fres.niter, fres.status]
+    alis_lines = get_alis_string(result, fres.params, fres.perror, info,
+                                 printout=False, getlines=True, save=False)
+
+    # Scan through the model to get parameter values and errors
+    alspl = alis_lines.split("\n")
+    for spl in range(len(alspl)):
+        if "gaussian" in alspl[spl]:
+            # Values
+            flag = 0
+            if "#" in alspl[spl]:
+                flag = 1
+        else:
+            continue
+        # Go through all the lines
+        if flag == 0:
+            pars[0], pars[1], pars[2] = float(alspl[1]), float(alspl[2]), float(alspl[3])
+        elif flag == 1:
+            errs[0], errs[1], errs[2] = float(alspl[2]), float(alspl[3]), float(alspl[4])
+    return pars, errs
+
+filename = "filename.fits"
+xst, yst = 100, 100   # Index of where to start fitting the data
+ist, zst, wst = 1000.0, 2.84/299792.458, 1.0E-3   # Initial guesses for starting point
+
+# Load cube
+dfil = fits.open(filename)
+flxdat = dfil[0].data
+fledat = dfil[1].data
+freq = 
+xsh, ysh = flxdat.shape[0], flxdat.shape[1]
+
+# Create a map of the order in which to fit the results
+xx, yy = np.meshgrid(np.arange(xsh)-xst, np.arange(ysh)-yst)
+ordmap = np.ma.MaskedArray(np.sqrt(xx**2 + yy**2), mask=np.zeros((xsh, ysh), dtype=np.int))
+
+# Initialise the intensity, velocity and dispersion maps
+imap = np.zeros((xsh, ysh, 2))
+vmap = np.zeros((xsh, ysh, 2))
+wmap = np.zeros((xsh, ysh, 2))
+
+# Perform the first fit
+flux = flxdat[xst,yst,:].flatten()
+flue = fledat[xst,yst,:].flatten()
+p0 = [ist,zst,wst]
+parlines, datlines, modlines = make_modeldata(freq, flux, flue, p0)
+result = alis(parlines=parlines, datlines=datlines, modlines=modlines, verbose=-1)
+pars, errs = parse_fit(result)
+imap[xst, yst, 0], vmap[xst, yst, 0], wmap[xst, yst, 0] = pars[0], pars[1], pars[2]
+imap[xst, yst, 1], vmap[xst, yst, 1], wmap[xst, yst, 1] = errs[0], errs[1], errs[2]
+ordmap[xst, yst] = np.ma.masked
+
+cntr = 1
+while cntr != xsh*ysh:
+    if cntr%100 == 0:
+        print(cntr, xsh*ysh)
+    # Find the next indice to fit
+    xind, yind = np.unravel_index(np.argmin(ordmap), (xsh,ysh))
+    # Extract the data
+    flux = flxdat[xind, yind, :].flatten()
+    flue = fledat[xind, yind, :].flatten()
+    # Find the starting parameters
+    xx, yy = np.meshgrid(np.arange(xsh)-xind, np.arange(ysh)-yind)
+    stpmap = np.ma.MaskedArray(np.sqrt(xx**2 + yy**2), mask=~ordmap.mask)
+    xp, yp = np.unravel_index(np.argmin(ordmap), (xsh,ysh))
+    p0 = [imap[xp,yp], vmap[xp,yp], wmap[xp,yp]]
+    parlines, datlines, modlines = make_modeldata(freq, flux, flue, p0)
+    # Perform the fit
+    result = alis(parlines=parlines, datlines=datlines, modlines=modlines, verbose=-1)
+    pars, errs = parse_fit(result)
+    # Store the result
+    imap[xind, yind, 0], vmap[xind, yind, 0], wmap[xind, yind, 0] = pars[0], pars[1], pars[2]
+    imap[xind, yind, 1], vmap[xind, yind, 1], wmap[xind, yind, 1] = errs[0], errs[1], errs[2]
+    ordmap[xind, yind] = np.ma.masked
+    if True:
+        # Do some checks along the way to make sure the image is getting filled in correctly
+        plt.clf()
+        plt.imshow(imap[:,:,0])
+        plt.show()
+    cntr += 1
+
+# Save the data
+np.save("Intensity", imap)
+np.save("Velocity", vmap)
+np.save("Dispersion", wmap)
+
+# Plot the results
+plt.subplot(131)
+plt.imshow(imap[:,:,0])
+plt.subplot(132)
+plt.imshow(vmap[:,:,0])
+plt.subplot(133)
+plt.imshow(wmap[:,:,0])
+plt.show()
---
+++ fit_disk.py
@@ -0,0 +1,138 @@
+import sys
+import time
+import emcee
+import numpy as np
+
+def rebin(a, shape):
+    sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+    return a.reshape(sh).mean(-1).mean(1)
+
+data = np.load("Velocity.npy")
+
+subgrid = 10
+y = data[:,:,0].flatten()
+ye = data[:,:,1].flatten()
+x = BLAH
+
+# Some good reference papers:
+# https://arxiv.org/pdf/1801.03948.pdf
+# https://iopscience.iop.org/article/10.1088/0004-637X/774/1/16/pdf
+# https://www.aanda.org/articles/aa/pdf/2006/11/aahk032.pdf
+# http://articles.adsabs.harvard.edu/cgi-bin/nph-iarticle_query?1986MNRAS.218..761H&amp;data_type=PDF_HIGH&amp;whole_paper=YES&amp;type=PRINTER&amp;filetype=.pdf
+#
+# List of model parameters:
+# vlsr  = line of sight velocity
+# Mstar = central mass
+# xcen  = x-position of central mass
+# ycen  = y-position of central mass
+# ang_i = angle of incidence
+# theta = angle on the observer's sky where the disk plane intersects the plane of the sky
+# dist  = Distance from observer to the disk
+# -------------------------------
+# Multi epoch parameters
+# pm_x  = proper motion in x
+# pm_y  = proper motion in y
+# accel = line of sight acceleration --  Delta[vlsr] / Delta[t_obs]
+
+# Coordinates
+# disk plane -- observer plane (see Rosenfeld (2013), ApJ, 774, 16; Eq 2-3)
+# x = x'
+# y = y'/cos(i)
+# z = 0
+# theta = arctan(x/y)
+# radius = sqrt(x**2 + y**2)
+# NOTE: x and x' both point along the major axis of the disk, according to the observer
+
+# Equations
+# vobs(x',y') = vlsr + sqrt(G*Mstar/radius) * np.sin(ang_i) * np.cos(theta)
+# Note, need to determine where xd points towards at every interation (i.e. cd must point towards the major axis [theta], which is determined at each iteration)
+# x', y' ==> xd, yd (where xd and yd are now in arcsecs on the sky)
+# vlsr ==> vlsr0 + accel*deltaT
+# xcen ==> xcen0 + pm_x
+# ycen ==> ycen0 + pm_y
+# radius ==> dist * sqrt((xd-xcen)**2 + ((yd-ycen)/cos(ang_i))**2)
+# theta ==> arctan((xd-xcen)*cos(ang_i)/(yd-ycen))
+
+def get_model(theta):
+    model = np.zeros(Ncol)
+    for ii in range(Ncol):
+        model[ii] = model_cden[ii]([[theta[1:]]])
+        if 'Si' in yn[ii]:
+            model[ii] += theta[0]
+    return model
+
+
+# Define the probability function as likelihood * prior.
+def lnprior(theta):
+    r, s, m, yy, n, h = theta
+    if mn_met <= m <= mx_met and \
+       mn_yp <= yy <= mx_yp and \
+       mn_hden <= n <= mx_hden and \
+       mn_NHI <= h <= mx_NHI and \
+       mn_sic <= r <= mx_sic and \
+       mn_slp <= s <= mx_slp:
+        return 0.0
+    return -np.inf
+
+
+def lnlike(theta, x, y, yerr):
+    model = get_model(theta).flatten()
+    inv_sigma2 = 1.0/yerr**2
+    return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
+
+
+def lnprob(theta, x, y, yerr):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlike(theta, x, y, yerr)
+
+
+# Find the maximum likelihood value by brute force.
+chi = np.zeros((model_yp.size, Ncol))
+for i in range(Ncol):
+    chi[:, i] = (y[i]-value_cden[i])/ye[i]
+chi2 = chi**2
+chisq = np.sum(chi2, axis=1)
+bst = np.argsort(chisq)
+printbst = 1
+for i in range(printbst):
+    print("""------------------------\n
+        Maximum likelihood result {5:d}/{6:d} {7:.4f}:\n
+        [M/H]  = {0}\n
+        yp     = {1}\n
+        n(H)   = {2}\n
+        N(H I) = {3}\n
+        slope  = {4}\n""".format(model_met[bst[i]], model_yp[bst[i]], model_hden[bst[i]], model_NHI[bst[i]],
+                                 model_slp[bst[i]], i+1, printbst, chisq[bst[i]]))
+modvals = [0.0, model_slp[bst[i]], model_met[bst[i]], model_yp[bst[i]], model_hden[bst[i]], model_NHI[bst[i]]]
+
+# Set up the sampler.
+ndim, nwalkers = 6, 100
+# maxlike = np.array([model_ms[bst], model_ex[bst], model_mx[bst]])
+# minv_ms, maxv_ms = 19.0, 22.0
+# minv_ex, maxv_ex = 1.0, 5.0
+# minv_mx, maxv_mx = 0.0, 0.0001
+# minv_ms, maxv_ms = np.min(model_ms[bst[:printbst]]), np.max(model_ms[bst[:printbst]])
+# minv_ex, maxv_ex = np.min(model_ex[bst[:printbst]]), np.max(model_ex[bst[:printbst]])
+# minv_mx, maxv_mx = np.min(model_mx[bst[:printbst]]), np.max(model_mx[bst[:printbst]])
+pos = [np.array([np.random.uniform(mn_sic, mx_sic),
+                 np.random.uniform(mn_slp, mx_slp),
+                 np.random.uniform(mn_met, mx_met),
+                 np.random.uniform(mn_yp, mx_yp),
+                 np.random.uniform(mn_hden, mx_hden),
+                 np.random.normal(y[0], ye[0])]) for i in range(nwalkers)]
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x, y, ye), threads=ndim)
+
+# Clear and run the production chain.
+print("Running MCMC...")
+nmbr = 15000
+a = time.time()
+for i, result in enumerate(sampler.run_mcmc(pos, nmbr, rstate0=np.random.get_state())):
+    if True:#(i+1) % 100 == 0:
+        print("{0:5.1%}".format(float(i) / nmbr))
+print("Done.")
+print((time.time()-a)/60.0, 'mins')
+
+print("Saving samples")
+np.save("diskfit.npy", sampler.chain)
